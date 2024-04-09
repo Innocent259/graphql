3716d54da955505fb396c20f097247c3835aa8e1
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-09 18:18:11 +0200

    Create resolve functions

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
 
 const server = new ApolloServer({
     typeDefs,
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
-console.log("Server is ready");
+console.log("Server is running at port", 4000);
